107ab82f315670c3ea79c96b06d3a6af5434132a
TyagMon1 <monish.tyagi@securiton-india.in>
2024-03-14 19:08:06 +0530

    14march24-1

---
+++ Angular/myApp/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { BooksComponent } from './books/books.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+const routes:Routes=[
+    {
+        path:'',component:BooksComponent
+    },
+    {
+        path:'cart',component:CartComponent
+    }
+    ,
+    {
+        path:'login',component:LoginComponent
+    }
+    ,
+    {
+        path:'register',component:RegisterComponent
+    }
+];
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+export class AppRoutingModule{}
--- Angular/myApp/src/app/books/books.component.ts
+++ Angular/myApp/src/app/books/books.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
  import { Book } from '../types/Book';
+import { BooksService } from './books.service';
 
 
 
@@ -8,7 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  books: Book[] = [];
+constructor(private booksService: BooksService) {
+}
+ngOnInit(): void {
+  this.books = this.booksService.getBooks();
+}
+
+
 name:string="Clean Code";
 author:string="Robert C. Martin";
 src:string="https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX258_BO1,204,203,200_.jpg";
@@ -16,48 +25,21 @@ name2:string="The Pragmatic Programmer";
 author2:string="David Thomas";
 src2:string="https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg";
 isDisabled:boolean=false;
-constructor(){}
-ngOnInit():void{}
+
 handleClick(){
   alert("Button Clicked")
   // this.isDisabled=!this.isDisabled;
 }
 myName:string="";
-isShow:boolean=false;
+isShow:boolean=true;
 
 handleInput(event:any)
 {
   this.myName=event.target.value;
 }
-addToCart(){
-  console.log('add')
-}
-books: Book[] = [
-  {
-    name: "clean code",
-    author: "Robert C. Martin",
-    image: "https://images-na.ssl-images-amazon.com/images/I/41jEbK-jG+L._SX258_BO1,204,203,200_.jpg",
-    amount:100
-  },
-  {
-    name: "The Pragmatic P rogrammer",
-    author: "David Thomas",
-    image: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
-    amount:200
-  }
-  ,
-  {
-    name: "The Pragmatic Programmer",
-    author: "David Thomas",
-    image: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
-    amount:200
-  }
-  ,
-  {
-    name: "The Pragmatic Programmer",
-    author: "David Thomas",
-    image: "https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg",
-    amount:200
-  }
-];
+// addToCart(book://   console.log('add')
+//   console.log(book)
+
+// }
+
 }
