d2bf49380aa4d0cb76bf52fd3c315b5d48a50dbe
MONISH_TYAGI <monishtyagi5770@gmail.com>
2024-03-15 01:34:02 +0530

    14march24-2

--- Angular/myApp/src/app/auth/login/login.component.ts
+++ Angular/myApp/src/app/auth/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import {LoginForm} from '../../types/auth';
+// import { AuthService } from '../auth.servi/ce';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-email:string="";
+form:LoginForm={
+  email:'',
+  password:'',
+}
+submit()
+{
+  console.log(this.form)
+}
 }
--- Angular/myApp/src/app/auth/register/register.component.ts
+++ Angular/myApp/src/app/auth/register/register.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { RegisterForm } from '../../types/auth';
+// import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  form:RegisterForm={
+    email:'',
+    password:'',
+    confirm_password:'',
+  }
+  submit()
+{
+  console.log(this.form)
+}
 }
